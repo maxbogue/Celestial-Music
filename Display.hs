41b80c597cb15952122ff18d9edc27df05a35cf3
Max Bogue <max@maxbogue.com>
2012-05-14 19:21:19 -0400

    Add some colors.

--- Display.hs
+++ Display.hs
@@ -7,6 +7,13 @@ windowPixels = 800 :: Integer
 windowScale  = 100 :: Float
 windowConversion = fromIntegral windowPixels / windowScale / 2
 
+circleColors :: [Color3 Float]
+circleColors = [
+    Color3 ( 15 / 255) ( 80 / 255) (170 / 255),
+    Color3 (190 / 255) (  0 / 255) (140 / 255),
+    Color3 (  0 / 255) (180 / 255) (240 / 255),
+    Color3 (255 / 255) (155 / 255) (  0 / 255)]
+
 renderCircle :: Float -> Float -> Float -> IO ()
 renderCircle d a r = do
     pointSize $= (r * windowConversion)
@@ -23,7 +30,11 @@ display spheresRef anglesRef = do
     (let s = 1 / windowScale in scale s s s)
     spheres <- get spheresRef
     angles <- get anglesRef
-    mapM_ (\(s, a) -> renderCircle (distance s) a 2) (zip spheres angles)
+    mapM_
+        (\(s, a, i) -> preservingMatrix $ do
+            color $ circleColors !! (i `mod` length circleColors)
+            renderCircle (distance s) a 2)
+        (zip3 spheres angles [1..])
     swapBuffers
 
 timer spheresRef anglesRef = do
