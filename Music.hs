2e0f84f68bd0ba278f964a1305fdbf39e94edba2
Max Bogue <max@maxbogue.com>
2012-05-15 01:46:45 -0400

    Make the music thing work by hacking Tomato into a new file.

--- Music.hs
+++ Music.hs
@@ -1,23 +1,39 @@
-import Sound.Tomato.Speakers
+module Music (initOpenAL, playSine) where
+
+import Control.Concurrent
+import Sound.OpenAL
+import Tomato
 
 waveProduct :: [Integer -> Float] -> [Float]
 waveProduct waveFuncs = map waveProduct' [0..] where
     waveProduct' t = foldl (*) 1 $ map (\f -> f t) waveFuncs
 
+sampleRate :: SampleRate
 sampleRate = 22050
 
 dt :: Float
-dt         = 1 / sampleRate
+dt = 1 / sampleRate
 
+sineWave :: Float -> Integer -> Float
 sineWave freq t = sin $ 2 * pi * freq * dt * fromIntegral t
 
-expDecay t = exp $ -3 * dt * fromIntegral t
+expDecay :: Float -> Integer -> Float
+expDecay d t = 10 ** (-4 * dt / d * fromIntegral t)
 
-playSine f = withSpeakers sampleRate 128 $ \s -> playSamples s sound
+playSine :: Float -> Float -> IO ThreadId
+playSine d f = forkIO $
+    withSpeakers sampleRate 128 $ \s -> playSamples s sound
   where
-    sound = take (ceiling $ 2 / dt)
-          $ waveProduct [expDecay, sineWave f]
+    sound = take (ceiling $ d / dt)
+          $ waveProduct [expDecay d, sineWave f]
           -- $ map ((* 0.5) . sineWave f) [0..]
 
+initOpenAL = do
+    Just device  <- openDevice Nothing
+    Just context <- createContext device []
+    currentContext $= Just context
+
 main = do
-    playSine 440
+    initOpenAL
+    playSine 1 440
+    threadDelay 1e6
